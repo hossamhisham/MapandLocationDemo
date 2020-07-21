d7d47005d6b0fbaff9a76a8629241dc9e8434ea2
hossam <https://github.com/hossamhisham>
2020-07-21 20:35:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/zappycode/mapandlocationdemo/UserLocation.java
@@ -0,0 +1,17 @@
+package com.example.zappycode.mapandlocationdemo;
+
+public class UserLocation {
+    // private string id;
+    private String address;
+    private double latitude;
+    private double longitude;
+
+    public UserLocation(String address, double latitude, double longitude){
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+
+    }
+
+}
